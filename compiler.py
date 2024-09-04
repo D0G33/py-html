26efb1530e59b89f211104cd5e7435a7fafd89f3
D0G33 <brysonrogersmith@gmail.com>
2024-09-04 09:48:45 -0500

    Html is weird

--- compiler.py
+++ compiler.py
@@ -1,10 +1,14 @@
-def rmSpaceWQ(string):
+from collections import defaultdict
+from email.policy import default
+
+
+def rmWQ(string,chara):
     inQuotes = False
     out = ""
     for i in string:
         if i == '"':
             inQuotes = not inQuotes
-        if inQuotes or i != " ":
+        if inQuotes or i != chara:
             out += i
 
     return out
@@ -22,37 +26,59 @@ def splitWQ(string,sep):
             out.append("")
     return out
 
-    return out
 def eCmd(line):
-    parts = line.split(".")
+    parts = splitWQ(line,".")
     out1 = parts[0]
-    out2 = parts[1].split("=")[0]
-    out3 = parts[1].split("=")[1].replace('"',"")
+    out2 = splitWQ(parts[1],"=")[0]
+    out3 = splitWQ(parts[1],"=")[1].replace('"',"")
     return [out1, out2, out3]
+
+def printDict(dict):
+    for i in dict:
+        print(i + ": " + dict[i])
+
 class htmlObj:
     def __init__(self,tag="p"):
         self.tag = tag
         self.content = ""
         self.attributes = {}
+        self.img = ""
     def __str__(self):
         atr = " "
         for i in self.attributes:
             atr += i + "=" + self.attributes[i]
             atr += " "
         if atr == " ": atr = ""
-
+        if self.img != "":
+            return "<" + self.tag + atr + ">" + self.img + self.content + "</" + self.tag + ">"
         return "<" + self.tag + atr + ">" + self.content + "</" + self.tag + ">"
     def __add__(self,other):
         return other + str(self)
     def __radd__(self,other):
         return other + str(self)
 
+class htmlImg:
+    def __init__(self,url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-RcH3_rFP8ZmSEgjhZy5pv4O4bLl-SwZGsA&s"):
+        self.tag = "img"
+        dStyle = "width:48px;height:48px"
+        self.attributes = {"src":url,"style":dStyle}
+
+    def __str__(self):
+        atr = " "
+        for i in self.attributes:
+            atr += i + "=" + self.attributes[i]
+            atr += " "
+        if atr == " ": atr = ""
+        return "<"+self.tag+ atr+">"
+    def __add__(self,other):
+        return other + str(self)
+    def __radd__(self,other):
+        return other + str(self)
 # Builtins
 link = htmlObj()
 link.tag = "a"
 link.attributes["href"] = "https://www.google.com"
 link.content = "Connect To Google!"
-
 
 class pyml:
     def __init__(self,dir):
@@ -68,8 +94,9 @@ class pyml:
         self.head = {"title":"PYML DEFAULT SCRIPT"}
         self.body = {}
 
-
+        self.images = {}
         self.references = {"link":link}
+
     def createFile(self,name="main"):
         f = open(name+".html",'w')
         for i in self.htmlHeaders:
@@ -91,11 +118,28 @@ class pyml:
             f.write(i + "\n")
 
     def populate(self):
-        noWhite = self.content.replace("\n","").replace("\t","")
-        keepQuotes = rmSpaceWQ(noWhite)
+        noWhite = rmWQ(self.content.replace("\t",""),'\n')
+        keepQuotes = rmWQ(noWhite," ")
         commands = keepQuotes.split(";")
         commands.pop(-1)
 
+        #Create Includes
+        for c, i in enumerate(commands):
+            if i[0] == ">":
+                f = open(i[1:]+".pyin",'r')
+                content = f.read()
+                f.close()
+                noWhite = rmWQ(content.replace("\t", ""), '\n')
+                keepQuotes = rmWQ(noWhite, " ")
+                externalCommands = keepQuotes.split(";")
+                externalCommands.pop(-1)
+                commands[c] = externalCommands[0]
+                exMf = externalCommands[1:]
+                for i in range(len(exMf)):
+                    commands.insert(c+1, exMf[len(exMf)-i-1])
+
+
+        #Remove Comments
         i = 0
         while i < len(commands):
             if commands[i][0] == "#":
@@ -119,6 +163,10 @@ class pyml:
                 self.comps.pop(i[1:])
                 print(i[1:] + " Moved to Refs")
 
+            elif i[0] == "+":
+                self.images[i[1:]] = htmlImg()
+                print(i[1:] + " Image Created")
+
             elif "<<" in i:
                 names = i.split("<<")
                 self.comps[names[0]] = self.references[names[1]]
@@ -133,10 +181,22 @@ class pyml:
                 else:
                     cmd = eCmd(i)
 
-                if cmd[1] == "content":
-                    self.comps[cmd[0]].content = cmd[2]
-                if cmd[1] == "tag":
-                    self.comps[cmd[0]].tag = cmd[2]
+                #Editting Image
+                if cmd[0] in self.images:
+                    print(cmd)
+                    if cmd[1] == "url":
+                        self.images[cmd[0]].attributes["src"] = cmd[2]
+                    if cmd[1] == "style":
+                        self.images[cmd[0]].attributes["style"] = cmd[2]
+
+                else:
+                    if cmd[1] == "content":
+                        self.comps[cmd[0]].content = cmd[2]
+                    if cmd[1] == "tag":
+                        self.comps[cmd[0]].tag = cmd[2]
+                    if cmd[1] == "img" or cmd[1] == "image" or cmd[1] == 'i':
+
+                        self.comps[cmd[0]].img = self.images[cmd[2]]
 
 
         print("\n\nComponents:")
